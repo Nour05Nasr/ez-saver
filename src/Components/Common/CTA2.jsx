bb7ab5459069fe510690753e521e64af172ba518
Nour05Nasr <nour.a.nasr05@gmail.com>
2026-03-30 05:08:34 +0200

    feat: create CTA1 & CTA2

---
+++ src/Components/Common/CTA2.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
+import './CTA2.css';
+
+const CTA2 = (props) => {
+    return ( 
+        <>
+            <Link className="cta_div2" to={props.url}>
+                <button className="cta_temp2">{props.title}</button>
+                {/* <div className="cta_bg"></div> */}
+            </Link>
+        </>
+     );
+}
+ 
+export default CTA2;
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import Nav from '../Components/Layout/Nav';
+import './Home.css';
+// import { Helmet } from 'react-helmet';
+
+const Home = () => {
+    return ( <div className='body_home'>
+     {/* <Helmet>
+        <title>NourNasrBackEndLogIn</title>
+        <meta name="robots" content="noindex, nofollow"></meta>
+        <meta name="description" content="This is the about page" />
+        <meta property="og:title" content="LogIn" />
+    </Helmet> */}
+
+    {/* <Nav /> */}
+   
+    
+    </div>);
+}
+export default Home;
